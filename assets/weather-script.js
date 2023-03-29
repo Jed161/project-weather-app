cf82136d149eaee70e5688a807a43516d4206736
James Edmond <jamesedmond@pursuit.org>
2023-03-29 18:23:09 -0400

    committing weather-app

--- assets/weather-script.js
+++ assets/weather-script.js
@@ -2,11 +2,12 @@ const weatherBox = document.getElementById("weather-box")
 const cityNameInput = document.getElementById("city-name-input")
 const searchButton = document.getElementById("search-button")
 
-searchButton.addEventListener("click", event => {
-    let cityNameInput = cityNameInput.value
-    console.log(cityName)
 
-    let receivedPromise = fetch(`https://wttr.in/${cityName}?format`)
+searchButton.addEventListener("click", event => {
+    let cityName = cityNameInput.value
+    cityNameInput.value =""
+
+    let receivedPromise = fetch(`https://wttr.in/${cityName}?format=j1`)
 
     receivedPromise.then(response => {
         return response.json()
@@ -15,9 +16,9 @@ searchButton.addEventListener("click", event => {
     })    
 })
 
-const fillWeatherBox = (json) => {
+const fillWeatherBox = (json, cityName) => {
     weatherBox.innerHTML = ""
-
+    
     let label = document.createElement("h3")
     label.textContent = cityName
     weatherBox.append(label)
@@ -31,13 +32,13 @@ const fillWeatherBox = (json) => {
     let regionName = json.nearest_area[0].region[0].value
     let region = document.createElement("li")
     region.className = `weather-box-item`
-    region.innerHTML = `<strong>Area:</strong> ${regionName}`
+    region.innerHTML = `<strong>Region:</strong> ${regionName}`
     weatherBox.append(region)
 
-    let countryName = json.nearest_area[0].countryName[0].value
+    let countryName = json.nearest_area[0].country[0].value
     let country = document.createElement("li")
     country.className = `weather-box-item`
-    country.innerHTML = `<strong>Area:</strong> ${countryName}`
+    country.innerHTML = `<strong>Country:</strong> ${countryName}`
     weatherBox.append(country)
 
     let temperatureValue = json.current_condition[0].FeelsLikeF
@@ -45,4 +46,27 @@ const fillWeatherBox = (json) => {
     temperature.className = `weather-box-item`
     temperature.innerHTML = `<strong>Currently:</strong> Feels like ${temperatureValue}°F`
     weatherBox.append(temperature)
+    
+    let previousList = document.querySelector(".previous-searches-list")
+    console.log(previousList)
+
+    const listItem = document.getElementById("previous-searches")
+    console.log(listItem)
+    listItem.remove()
+
+if (!previousList.innerHTML.includes(json.nearest_area[0].areaName[0].value)) {
+    previousList.innerHTML += `<a href="#">${cityName}</a>-${temperatureValue}`
+}
+
+    // let previous = document.createElement("ul")
+    // console.log(previous)
+
+    // if(listItem.textContent === "No previous searches.") {
+    //     listItem.remove()
+    //     listItem.innerHTML = `<a href="#">${areaName}</a>-${temperatureValue}`
+    //     weatherBox.append(listItem)
+    // } else {
+    //     listItem.innerHTML = `<a href="#">${cityName}</a>-${temperatureValue}`
+    // }
+    // weatherBox.append(listItem)
 }
